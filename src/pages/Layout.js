f57ec56c4d0e95848d9d02678af0f9a6f3103d7c
vishwaskapte <45562090+vishwaskapte@users.noreply.github.com>
2023-12-20 18:44:46 +0530

    netlify fixed 3

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -14,10 +14,10 @@ export default function Layout() {
                   </button>
                   <div className="collapse navbar-collapse" id="navbarResponsive">
                       <ul className="navbar-nav ms-auto py-4 py-lg-0">
-                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/">Home</Link></li>
-                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/about">About</Link></li>
-                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/post">Sample Post</Link></li>
-                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" href="/contact">Contact</Link></li>
+                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/">Home</Link></li>
+                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/about">About</Link></li>
+                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/post">Sample Post</Link></li>
+                          <li className="nav-item"><Link className="nav-link px-lg-3 py-3 py-lg-4" to="/contact">Contact</Link></li>
                       </ul>
                   </div>
               </div>
